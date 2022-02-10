56a481af82ebc6ddf58d4c3b82341559f2f352cf
lilobanova <lilobanova10@gmail.com>
2022-02-10 13:52:30 +0300

    Add ex

---
+++ py14/ex1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+def add_two(a):
+    x = 2
+    return a + x
+
+
+if __name__ == "__main__":
+    print(add_two(3))
+    print(x)
---
+++ py14/ex3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+x = 4
+
+
+def fun():
+    print(x + 3)
+
+
+if __name__ == "__main__":
+    fun()
